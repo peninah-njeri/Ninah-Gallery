e06383e13ed08ea43d6f67b0b6b504962313474a
peninah-njeri <peninahgathuru@gmail.com>
2019-11-08 15:29:16 +0300

    define the applicatios configurations inside the project so as to connect to the templates

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
 
 # Create your views here.
+
+def welcome(request):
+    return HttpResponse('God creates the beauty. My camera and i are a witness.')
+
