1267bd643776a6034a928a6f2c0634d45e15d678
peninah-njeri <peninahgathuru@gmail.com>
2019-11-09 12:44:46 +0300

    create a database that will store data for the project

--- Personal_Gallery/settings.py
+++ Personal_Gallery/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'Personal_Gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Personal_Gallery',
+        'USER':'pesh',
+        'PASSWORD':'peninah',
     }
 }
 
