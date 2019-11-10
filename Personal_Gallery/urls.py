12114a268f853593cdac72bd14ccdc0ea0bfe3b0
peninah-njeri <peninahgathuru@gmail.com>
2019-11-10 19:46:20 +0300

    changes url path for the home route

--- Personal_Gallery/urls.py
+++ Personal_Gallery/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-     path('gallery/',include('gallery.urls')),
+    path('',include('gallery.urls')),
     path('admin/', admin.site.urls), 
 ]
 
