642c6121a6c8176fe680b3ac7bb41c299cf7f657
peninah-njeri <peninahgathuru@gmail.com>
2019-11-10 13:38:10 +0300

    Define model relationships and migrate to save changes

--- gallery/models.py
+++ gallery/models.py
@@ -1,13 +1,30 @@
 from django.db import models
 
 # Create your models here.
+    
+class Location(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
+
+    
 
 class Image(models.Model):
+    image = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/None/no-img.jpg')
     name = models.CharField(max_length =30)
     description = models.TextField(max_length=400)
-    
-    
-
+    location = models.ForeignKey(Location, blank=True, null=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, null=True, on_delete=models.CASCADE)
 
 
 
